b43c5dfda707a7bcdb8a26926b308b1401dcff3b
Dennis Osido <dennisosido@yahoo.com.ph>
2020-04-16 20:27:10 +0800

    Added my_each method

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -1,4 +1,20 @@
 # rubocop:disable Style/CaseEquality
+module Enumerable
+	
+	def my_each
+		return to_enum(:my_each) unless block_given?
+		i = 0
+		while i < self.size do 
+			yield self[i]
+			i += 1
+		end
+		self  
+	end
 
-
+end    
 # rubocop:enable Style/CaseEquality
+
+[3, 6, 9, 12].my_each do |n| 
+	n /= 3
+  puts "Current number divided by 3 is: #{n}"
+end
