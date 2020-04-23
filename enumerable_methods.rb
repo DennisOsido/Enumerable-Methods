eed0fbb79c4c2eafd62e733b5adddcf4517cf507
Dennis Osido <dennisosido@yahoo.com.ph>
2020-04-23 13:22:03 +0800

    Fixed Rubocop errors and removed test cases from rb file

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -1,115 +1,102 @@
-# rubocop:disable Style/CaseEquality
+# rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
 module Enumerable
+  def my_each
+    return to_enum(:my_each) unless block_given?
 
-	def my_each
-		return to_enum(:my_each) unless block_given?
-		i = 0
-		while i < self.size do
-			yield self[i]
-			i += 1
-		end
-		self
-	end
+    i = 0
+    while i < size
+      yield self[i]
+      i += 1
+    end
+    self
+  end
 
-	def my_each_with_index
+  def my_each_with_index
     return to_enum(:my_each_with_index) unless block_given?
-		i = 0
-		index = 0
-    while i < self.size do
+
+    i = 0
+    while i < size
       yield(self[i], i)
       i += 1
     end
     self
   end
 
-	def my_select
+  def my_select
     return to_enum(:my_select) unless block_given?
+
     selected_array = []
     my_each do |element|
-      if yield(element)
-        selected_array << element
-      end
+      selected_array << element if yield(element)
     end
     selected_array
   end
 
-  def my_all?(p = nil)
+  def my_all?(_para = nil)
     result = true
     if block_given?
-      my_each do |element| 
+      my_each do |element|
         result = false unless yield(element)
       end
     else
-      my_each do |element| 
+      my_each do |element|
         result = false unless element
       end
     end
     result
   end
 
-  def my_any?(p = nil)
-    return my_any?(p) if block_given? && !p.nil?
+  def my_any?(para = nil)
+    return my_any?(para) if block_given? && !para.nil?
+
     if block_given?
       to_a.my_each do |element|
-        if yield element
-          return true
-        end  
-      false
+        return true if yield element
+
+        false
       end
-    elsif p
+    elsif para
       to_a.my_each do |element|
-        if element == p
-          return true
-        end  
-      end  
-    elsif p.nil?
-      to_a.my_each do |element|  
-        if element
-          return true
-        end  
+        return true if element == para
+      end
+    elsif para.nil?
+      to_a.my_each do |element|
+        return true if element
       end
     end
     false
   end
 
-  def my_none?(p = nil)
-    return my_none?(p) if block_given? && !p.nil?
+  def my_none?(para = nil)
+    return my_none?(para) if block_given? && !para.nil?
+
     if block_given?
       to_a.my_each do |element|
-        if yield element
-          return false
-        end  
-      false
+        return false if yield element
+
+        false
       end
-    elsif p
+    elsif para
       to_a.my_each do |element|
-        if element == p
-          return false
-        end  
-      end  
-    elsif p.nil?
-      to_a.my_each do |element|  
-        if element
-          return false
-        end  
+        return false if element == para
+      end
+    elsif para.nil?
+      to_a.my_each do |element|
+        return false if element
       end
     end
     true
   end
 
-  def my_count(p = nil)
+  def my_count(para = nil)
     count = 0
     if block_given?
       my_each do |element|
-        if yield(element)
-          count += 1 
-        end  
+        count += 1 if yield(element)
       end
-    elsif p
+    elsif para
       my_each do |element|
-        if element == p
-          count += 1 
-        end  
+        count += 1 if element == para
       end
     else
       count = size
@@ -122,128 +109,37 @@ module Enumerable
     if prc
       my_each do |elem|
         arr << prc.call(elem)
-      end  
+      end
     else
-      my_each do |elem| 
+      my_each do |elem|
         arr << yield(elem)
-      end  
+      end
     end
     arr
   end
 
-  def my_inject(p1 = nil, p2 = nil)
-    array = is_a?(Array) ? self:to_a
-    if p1.is_a?(Symbol) or p1.is_a?(String)
-      symbol = p1
+  def my_inject(para1 = nil, para2 = nil)
+    array = is_a?(Array) ? self : to_a
+    symbol = para1 if para1.is_a?(Symbol) or para1.is_a?(String)
+    if para1.is_a?(Integer)
+      accumulate = para1
+      symbol = para2 if para2.is_a?(Symbol) or para2.is_a?(String)
     end
-  
-    if p1.is_a?(Integer)
-      accumulate = p1
-      if p2.is_a?(Symbol) or p2.is_a?(String)
-        symbol = p2
-      end  
-    end
-  
     if symbol
-      array.my_each do |i| 
+      array.my_each do |i|
         accumulate = accumulate ? accumulate.send(symbol, i) : i
-      end  
+      end
     elsif block_given?
-      array.my_each do |i| 
+      array.my_each do |i|
         accumulate = accumulate ? yield(accumulate, i) : i
-      end  
+      end
     end
     accumulate
   end
-  
+
   def multiply_els(array)
-  array.my_inject(:*)
-  end
-
-end
-# rubocop:enable Style/CaseEquality
-
-puts
-puts "my_each method"
-
-nums = [2, 4, 6, 8]
-nums.my_each do |n|
-	n /= 2
-  puts "#{nums[n - 1]} divided by 2 is: #{n}"
-end
-
-puts
-
-puts "my_each_with_index method"
-[2, 4, 6, 8].my_each_with_index do |n, index|
-	puts "Index: #{index} | Value: #{n}"
-end
-
-puts
-
-puts "my_select method"
-nums_array = [5, -4, 0, 9]
-print "Sample Array of Numbers: #{nums_array}"
-puts
-nums_array.my_select do |x|
-  if x.positive?
-    puts "#{x} is positive"
+    array.my_inject(:*)
   end
 end
 
-puts
-
-puts "my_all? method"
-nums = [13, 7, 24, 39]
-print nums
-puts
-print "All are odd? = "
-print nums.my_all? { |n| n.odd? }
-puts
-puts
-
-puts "my_any? method"
-nums = [13, 7, 25, 38]
-print nums
-puts
-print "Is any even? = "
-print nums.my_any? { |n| n.even? }
-puts
-puts
-
-puts "my_none? method"
-nums = [13, 7, 25, 39]
-print nums
-puts
-print "None are even? = "
-print nums.my_none? { |n| n.even? }
-puts
-puts
-
-puts "my_count method"
-nums = [13, 8, 24, 39]
-print nums
-puts
-print "How many are even? = "
-print nums.my_count { |n| n.even? }
-puts
-puts
-
-puts "my_map method"
-nums = [2, 4, 6, 8]
-print nums
-puts
-nums.my_map do |n|
-  n * 2
-  puts "#{n} multiplied by 2 is: #{n}"
-end
-puts
-
-puts "my_inject method"
-nums = [1, 2, 3, 4]
-print nums
-puts
-print "my_inject add: "
-print nums.my_inject("+") 
-puts
-puts
+# rubocop:enable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
